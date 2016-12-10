05c302f00ccad9d1334fdd9a3e04d10a4565176d
usman-tahir <tahir.usman.ali94@outlook.com>
2016-12-10 12:22:52 -0500

    developed the CourseComponent template and added a list of boilerplate courses

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -3,6 +3,21 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>Courses</h2>
+        {{ title }}
+        <ul>
+            <li *ngFor="#course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        `
 })
-export class CoursesComponent { }
+export class CoursesComponent { 
+    title = 'The title of the Courses page';
+    courses = [
+        'Course one',
+        'Course two',
+        'Course three'
+    ];
+}
